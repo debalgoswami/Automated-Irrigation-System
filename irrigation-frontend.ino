e6fa29b12d8f14797cfb72b26e5e8f255b400b0b
Soham Adhikari <69967401+sohamadhikari@users.noreply.github.com>
2020-10-16 11:10:26 +0530

    Added LDR reading feature

--- irrigation-frontend.ino
+++ irrigation-frontend.ino
@@ -13,3 +13,11 @@ const char *pass =  "Password"; // Enter your WiFi Password
 Adafruit_MQTT_Publish AgricultureData = Adafruit_MQTT_Publish(&mqtt,MQTT_NAME "/f/AgricultureData"); //publishing feed setup
 Adafruit_MQTT_Subscribe LED = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "/f/LED");
 Adafruit_MQTT_Subscribe Pump = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "/f/Pump"); //subscribed feed setup
+
+int ldrStatus = analogRead(ldrPin);  //LDR reading
+if (ldrStatus <= 200) {
+    digitalWrite(ledPin, HIGH);
+} 
+else {
+   digitalWrite(ledPin, LOW);
+}
